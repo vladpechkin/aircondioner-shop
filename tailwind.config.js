b1f938388848ed56ebd928b3f6143df6fca16a4c
Vlad <pechkinde@gmail.com>
2022-08-25 12:48:29 +0400

    Rewrite on clean html+js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,7 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {
-      fontFamily: {
-        alegreya: ["Alegreya", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
